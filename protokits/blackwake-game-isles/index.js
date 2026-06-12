abdb3e7da3f7b32635ffe578a98a6500b2268c55
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-11 21:43:50 -0400

    Add Blackwake Isles game ProtoKit wrapper

---
+++ protokits/blackwake-game-isles/index.js
@@ -0,0 +1,17 @@
+import {
+  createBlackwakeIslesGame,
+  createBlackwakeIslesProtoKit,
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits
+} from "../blackwake-kit-registry/index.js";
+
+export {
+  createBlackwakeIslesGame,
+  createBlackwakeIslesProtoKit,
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits
+};
+
+export const BLACKWAKE_ISLES_GAME_ID = "blackwake-game-isles";
