ee5c9317d73ad1e5bac108f5967e95a6b3e0a107
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-12 00:11:15 -0400

    Add Blackwake Isles playable wrapper to main

---
+++ protokits/blackwake-game-isles/index.js
@@ -0,0 +1,26 @@
+import {
+  createBlackwakeIslesGame,
+  createBlackwakeIslesProtoKit,
+  createBlackwakePlayableGame,
+  createBlackwakeHealthReport
+} from "../blackwake-gameplay/index.js";
+
+import {
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits,
+  validateBlackwakeProtoKitGraph
+} from "../blackwake-kit-registry/index.js";
+
+export {
+  createBlackwakeIslesGame,
+  createBlackwakeIslesProtoKit,
+  createBlackwakePlayableGame,
+  createBlackwakeHealthReport,
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits,
+  validateBlackwakeProtoKitGraph
+};
+
+export const BLACKWAKE_ISLES_GAME_ID = "blackwake-game-isles";
