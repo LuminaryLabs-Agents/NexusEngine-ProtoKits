22ae17bdd24175b089357e28b7c69bf0e8c169fa
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-12 00:11:30 -0400

    Add Stormline Rescue playable wrapper to main

---
+++ protokits/blackwake-game-stormline-rescue/index.js
@@ -0,0 +1,26 @@
+import {
+  createStormlineRescueGame,
+  createStormlineRescueProtoKit,
+  createBlackwakePlayableGame,
+  createBlackwakeHealthReport
+} from "../blackwake-gameplay/index.js";
+
+import {
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits,
+  validateBlackwakeProtoKitGraph
+} from "../blackwake-kit-registry/index.js";
+
+export {
+  createStormlineRescueGame,
+  createStormlineRescueProtoKit,
+  createBlackwakePlayableGame,
+  createBlackwakeHealthReport,
+  createBlackwakeProtoKit,
+  createBlackwakeRuntimeKits,
+  listBlackwakeProtoKits,
+  validateBlackwakeProtoKitGraph
+};
+
+export const STORMLINE_RESCUE_GAME_ID = "blackwake-game-stormline-rescue";
