297f437dde660281efaf0824156f7acc453d387f
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:57:30 -0400

    Add render culling system kit

---
+++ protokits/render-culling-system-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createRenderCullingSystemKit } from "../adaptive-visual-core/index.js";
+
+export const RENDER_CULLING_SYSTEM_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createRenderCullingSystemKit };
+export default createRenderCullingSystemKit;
