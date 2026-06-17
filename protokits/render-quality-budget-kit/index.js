b76af2cea45e1f7a22db0414a9b7f3d3373fda0b
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:56:25 -0400

    Add render quality budget kit

---
+++ protokits/render-quality-budget-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createRenderQualityBudgetKit } from "../adaptive-visual-core/index.js";
+
+export const RENDER_QUALITY_BUDGET_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createRenderQualityBudgetKit };
+export default createRenderQualityBudgetKit;
