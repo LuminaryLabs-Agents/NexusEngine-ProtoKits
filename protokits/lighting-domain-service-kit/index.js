f285c6e9e854e5b4187f5efbeb6bc9b7aebe77fe
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:57:03 -0400

    Add lighting domain service kit

---
+++ protokits/lighting-domain-service-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createLightingDomainServiceKit } from "../adaptive-visual-core/index.js";
+
+export const LIGHTING_DOMAIN_SERVICE_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createLightingDomainServiceKit };
+export default createLightingDomainServiceKit;
