e046e7724c0e6885718bb92cdba321db1b0a4ef4
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:58:02 -0400

    Add asset quality kit

---
+++ protokits/asset-quality-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createAssetQualityKit } from "../adaptive-visual-core/index.js";
+
+export const ASSET_QUALITY_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createAssetQualityKit };
+export default createAssetQualityKit;
