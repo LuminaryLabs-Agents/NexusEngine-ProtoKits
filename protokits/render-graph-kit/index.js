5152691df1a8e3f0731e4d9889de9299dec5818d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:56:37 -0400

    Add render graph kit

---
+++ protokits/render-graph-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createRenderGraphKit } from "../adaptive-visual-core/index.js";
+
+export const RENDER_GRAPH_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createRenderGraphKit };
+export default createRenderGraphKit;
