6d0e3f42558692ab05fb891a27e47205e25d0075
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:58:16 -0400

    Add webgpu render adapter kit

---
+++ protokits/webgpu-render-adapter-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createWebgpuRenderAdapterKit } from "../adaptive-visual-core/index.js";
+
+export const WEBGPU_RENDER_ADAPTER_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createWebgpuRenderAdapterKit };
+export default createWebgpuRenderAdapterKit;
