6381d0311b7bc7012f23e582cf33b2a837139a02
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:58:10 -0400

    Add webgl render adapter kit

---
+++ protokits/webgl-render-adapter-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createWebglRenderAdapterKit } from "../adaptive-visual-core/index.js";
+
+export const WEBGL_RENDER_ADAPTER_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createWebglRenderAdapterKit };
+export default createWebglRenderAdapterKit;
