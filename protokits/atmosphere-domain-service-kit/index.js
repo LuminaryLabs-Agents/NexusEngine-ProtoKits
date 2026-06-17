922da2055fb0507b59387488a2cb659dc55964b7
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:57:15 -0400

    Add atmosphere domain service kit

---
+++ protokits/atmosphere-domain-service-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createAtmosphereDomainServiceKit } from "../adaptive-visual-core/index.js";
+
+export const ATMOSPHERE_DOMAIN_SERVICE_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createAtmosphereDomainServiceKit };
+export default createAtmosphereDomainServiceKit;
