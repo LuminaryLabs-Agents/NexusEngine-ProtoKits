fbf5566d27c8a82b9ae644524d637f8dccb6faea
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:56:15 -0400

    Add render capability kit

---
+++ protokits/render-capability-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createRenderCapabilityKit } from "../adaptive-visual-core/index.js";
+
+export const RENDER_CAPABILITY_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createRenderCapabilityKit };
+export default createRenderCapabilityKit;
