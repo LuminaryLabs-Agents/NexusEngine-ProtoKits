05f50c2495a96dd8e6de5cb771a2923e14569685
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:57:41 -0400

    Add LOD selection system kit

---
+++ protokits/lod-selection-system-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createLodSelectionSystemKit } from "../adaptive-visual-core/index.js";
+
+export const LOD_SELECTION_SYSTEM_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createLodSelectionSystemKit };
+export default createLodSelectionSystemKit;
