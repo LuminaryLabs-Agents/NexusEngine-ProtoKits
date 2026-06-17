db2d5fa33c72baa1d14ca577d7bbfae1d84e69ea
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-17 18:56:52 -0400

    Add material visual domain service kit

---
+++ protokits/material-domain-service-kit/index.js
@@ -0,0 +1,5 @@
+import { ADAPTIVE_VISUAL_STACK_VERSION, createMaterialDomainServiceKit } from "../adaptive-visual-core/index.js";
+
+export const MATERIAL_DOMAIN_SERVICE_KIT_VERSION = ADAPTIVE_VISUAL_STACK_VERSION;
+export { createMaterialDomainServiceKit };
+export default createMaterialDomainServiceKit;
