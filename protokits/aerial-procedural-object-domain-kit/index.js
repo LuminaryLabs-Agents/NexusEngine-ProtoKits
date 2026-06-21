eda57f60369d429795b079fa83deb79549d5b59a
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-21 04:32:06 -0400

    Add aerial procedural object domain kit export

---
+++ protokits/aerial-procedural-object-domain-kit/index.js
@@ -0,0 +1,1 @@
+export { AERIAL_PROCEDURAL_OBJECT_DOMAIN_KIT_VERSION, createAerialProceduralObjectDomainKit, createAerialProceduralObjectDomainKit as default } from "../aerial-canyon-kits/index.js";
