bcb939ec1585a2375d09a11bcffbc609c100bc76
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-21 08:18:36 -0400

    Add async kit bundle

---
+++ protokits/async-kit-bundle/index.js
@@ -0,0 +1,8 @@
+export { createAsyncDomainLoadKit } from "../async-domain-load-kit/index.js";
+export { createKitLoadPlanKit } from "../kit-load-plan-kit/index.js";
+export { createAssetLoadQueueKit } from "../asset-load-queue-kit/index.js";
+export { createObjaverseCatalogKit } from "../objaverse-catalog-kit/index.js";
+export { createObjaverseMetadataIndexKit } from "../objaverse-metadata-index-kit/index.js";
+export { createObjaverseMetadataStreamKit } from "../objaverse-metadata-stream-kit/index.js";
+export { createObjaverseMeshRequestKit } from "../objaverse-mesh-request-kit/index.js";
+export { createObjaverseMeshCacheKit } from "../objaverse-mesh-cache-kit/index.js";
