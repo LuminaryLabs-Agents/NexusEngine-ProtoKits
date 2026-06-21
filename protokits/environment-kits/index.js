83a5ff8018cef9c1c0439312bb3aab539302cfd0
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-21 04:34:01 -0400

    Add environment kits aggregate export

---
+++ protokits/environment-kits/index.js
@@ -0,0 +1,4 @@
+export { BIOME_FIELD_KIT_VERSION, createBiomeField, createBiomeFieldKit } from "../biome-field-kit/index.js";
+export { VEGETATION_ARCHETYPE_KIT_VERSION, createVegetationArchetypeLibrary, createVegetationArchetypeKit } from "../vegetation-archetype-kit/index.js";
+export { GROUND_CONTACT_KIT_VERSION, createGroundContactService, createGroundContactKit, seatOnGround, slopeFromNormal, applyGroundInset, alignToNormal, withSlopeValidity } from "../ground-contact-kit/index.js";
+export { VEGETATION_LOD_KIT_VERSION, createVegetationLodSelector, createVegetationLodKit } from "../vegetation-lod-kit/index.js";
