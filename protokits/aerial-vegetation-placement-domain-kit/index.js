e79b6d1e01f10822d78d55c5e63baf593fb93529
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-21 04:31:53 -0400

    Add aerial vegetation placement domain kit export

---
+++ protokits/aerial-vegetation-placement-domain-kit/index.js
@@ -0,0 +1,1 @@
+export { AERIAL_VEGETATION_PLACEMENT_DOMAIN_KIT_VERSION, createAerialVegetationPlacementDomainKit, createAerialVegetationPlacementDomainKit as default } from "../aerial-canyon-kits/index.js";
