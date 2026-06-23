62b0d7f488f969a91428bf2b1a9b8ba0851beb9f
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-23 14:33:37 -0400

    Add generic defense AAA DSK bridge

---
+++ protokits/generic-defense-aaa-dsk-bridge/index.js
@@ -0,0 +1,34 @@
+export {
+  GENERIC_DEFENSE_AAA_KITS_VERSION,
+  createGenericDefenseKits,
+  createGenericDefenseGame,
+  createGenericDefenseFoundationKit,
+  createGenericDefenseWorldKit,
+  createGenericDefenseBuildKit,
+  createGenericDefenseCombatKit,
+  createGenericDefenseAgentKit,
+  createGenericDefenseWaveKit,
+  createGenericDefenseEconomyKit,
+  createGenericDefenseAbilityKit,
+  createGenericDefenseObjectiveKit,
+  createGenericDefensePresentationKit,
+  createGenericDefenseScaleKit,
+  createGenericDefenseAuthoringQaKit,
+  createGenericDefenseLevel
+} from "../generic-defense-aaa-kits/index.js";
+
+export {
+  GENERIC_DEFENSE_DSK_BOUNDARIES,
+  createGenericDefenseDskBundle,
+  createGenericDefenseMapDsk,
+  createGenericDefenseEconomyWalletDsk,
+  createGenericDefenseBuildPlacementDsk,
+  createGenericDefenseWaveAgentDirectorDsk,
+  createGenericDefenseCombatResolverDsk,
+  createGenericDefenseSessionFacadeDsk,
+  createGenericDefenseRenderDescriptorDsk,
+  getGenericDefenseDskBoundary,
+  listGenericDefenseDskBoundaries
+} from "../generic-defense-dsk-boundaries/index.js";
+
+export { default } from "../generic-defense-aaa-kits/index.js";
