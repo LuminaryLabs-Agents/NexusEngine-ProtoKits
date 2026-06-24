f86e8c2acd9d06ebe32c239f06c57813188f66a7
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 06:36:48 -0400

    Add platformer level kit entrypoint

---
+++ protokits/platformer-level-domain-kit/index.js
@@ -0,0 +1,1 @@
+export { createPlatformerLevelDomainKit as default, createPlatformerLevelDomainKit, DEFAULT_PLATFORMER_LEVEL, VR_PLATFORMER_KIT_SUITE_VERSION } from "../vr-platformer-kit-suite/index.js";
