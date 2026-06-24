f30220f367df4c9681cc66218b2c536e04476e3b
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 02:57:44 -0400

    Add XR house demo composition kit

---
+++ protokits/xr-house-demo-kit/index.js
@@ -0,0 +1,15 @@
+export function createXrHouseDemoKit(config = {}) {
+  return {
+    id: "xr-house-demo-kit",
+    kind: "composition-kit",
+    version: "0.1.0",
+    config,
+    kits: [
+      "xr-input-kit",
+      "xr-grab-throw-kit",
+      "simple-rigid-body-kit",
+      "toon-visual-kit",
+      "sky-gradient-kit"
+    ]
+  };
+}
