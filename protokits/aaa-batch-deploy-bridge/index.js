0e829af22b495cb0ce18df2853af6510b2a1fc0d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 00:01:32 -0400

    Fix legacy batch deploy bridge aliases

--- protokits/aaa-batch-deploy-bridge/index.js
+++ protokits/aaa-batch-deploy-bridge/index.js
@@ -1,12 +1,20 @@
+import {
+  PROJECT_BATCH_DEPLOY_BRIDGE_VERSION,
+  convertProjectBatchItemToDeployManifest,
+  convertProjectBatchToDeployManifests,
+  createProjectBatchDeployBridge
+} from "../project-batch-deploy-bridge/index.js";
+
 export {
   PROJECT_BATCH_DEPLOY_BRIDGE_VERSION,
   convertProjectBatchItemToDeployManifest,
   convertProjectBatchToDeployManifests,
-  createProjectBatchDeployBridge,
-  default
+  createProjectBatchDeployBridge
 } from "../project-batch-deploy-bridge/index.js";
 
 export const AAA_BATCH_DEPLOY_BRIDGE_VERSION = PROJECT_BATCH_DEPLOY_BRIDGE_VERSION;
 export const convertAaaBatchGameToDeployManifest = convertProjectBatchItemToDeployManifest;
 export const convertAaaBatchGamesToDeployManifests = convertProjectBatchToDeployManifests;
 export const createAaaBatchDeployBridge = createProjectBatchDeployBridge;
+
+export default createProjectBatchDeployBridge;
