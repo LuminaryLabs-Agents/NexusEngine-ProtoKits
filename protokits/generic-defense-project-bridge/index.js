3abc004baf0d94ab9754d708889218da66a963be
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 00:02:44 -0400

    Add generic defense project bridge alias

---
+++ protokits/generic-defense-project-bridge/index.js
@@ -0,0 +1,35 @@
+export {
+  GENERIC_DEFENSE_PROJECT_KITS_VERSION,
+  createGenericDefenseKits,
+  createGenericDefenseGame,
+  createGenericDefenseFoundationKit,
+  createGenericDefenseWorldKit,
+  createGenericDefenseBuildKit,
+  createGenericDefenseCombatKit,
+  createGenericDefenseAgentKit,
+  createGenericDefenseWaveKit,
+  createGenericDefenseEconomyKit,
+  createGenericDefenseAbilityKit,
+  createGenericDefenseObjectiveKit,
+  createGenericDefensePresentationKit,
+  createGenericDefenseScaleKit,
+  createGenericDefenseAuthoringQaKit,
+  createGenericDefenseLevel,
+  default
+} from "../generic-defense-project-kits/index.js";
+
+export {
+  GENERIC_DEFENSE_DSK_ENGINE_NAMESPACE,
+  GENERIC_DEFENSE_DSK_BOUNDARIES,
+  createGenericDefenseDskBundle,
+  createGenericDefenseMapDsk,
+  createGenericDefenseEconomyWalletDsk,
+  createGenericDefenseBuildPlacementDsk,
+  createGenericDefenseWaveAgentDirectorDsk,
+  createGenericDefenseCombatResolverDsk,
+  createGenericDefenseSessionFacadeDsk,
+  createGenericDefenseRenderDescriptorDsk,
+  getGenericDefenseDskBoundary,
+  listGenericDefenseDskBoundaries,
+  syncGenericDefenseDskEngineNamespace
+} from "../generic-defense-dsk-boundaries/index.js";
