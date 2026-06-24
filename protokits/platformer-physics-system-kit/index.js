0801373ab5af1f88adc9b0f542b5bb2ed2ccba5b
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 06:37:14 -0400

    Add platformer physics kit entrypoint

---
+++ protokits/platformer-physics-system-kit/index.js
@@ -0,0 +1,1 @@
+export { createPlatformerPhysicsSystemKit as default, createPlatformerPhysicsSystemKit, simulatePlatformerStep, VR_PLATFORMER_KIT_SUITE_VERSION } from "../vr-platformer-kit-suite/index.js";
