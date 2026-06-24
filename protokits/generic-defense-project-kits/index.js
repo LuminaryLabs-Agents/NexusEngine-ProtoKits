4132a06aea595f492b9a43b94d591e4d4252417c
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-24 00:02:33 -0400

    Add generic defense project kits alias

---
+++ protokits/generic-defense-project-kits/index.js
@@ -0,0 +1,19 @@
+export {
+  GENERIC_DEFENSE_AAA_KITS_VERSION as GENERIC_DEFENSE_PROJECT_KITS_VERSION,
+  createGenericDefenseKits,
+  createGenericDefenseGame,
+  createGenericDefenseFoundationKit,
+  createGenericDefenseWorldKit,
+  createGenericDefenseBuildKit,
+  createGenericDefenseCombatKit,
+  createGenericDefenseAgentKit,
+  createGenericDefenseWaveKit,
+  createGenericDefenseEconomyKit,
+  createGenericDefenseAbilityKit,
+  createGenericDefenseObjectiveKit,
+  createGenericDefensePresentationKit,
+  createGenericDefenseScaleKit,
+  createGenericDefenseAuthoringQaKit,
+  createGenericDefenseLevel,
+  default
+} from "../generic-defense-aaa-kits/index.js";
