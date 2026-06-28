fdb4d67fdec3ba04b3cd37df56fea601cf7332aa
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 09:35:19 -0400

    Add generic visual FX descriptor exports

---
+++ protokits/generic-visual-fx-descriptor-kits/index.js
@@ -0,0 +1,6 @@
+export { GENERIC_VISUAL_FX_DESCRIPTOR_KITS_VERSION, createGenericVisualFxDescriptorKits } from "./visual-fx-bundle.js";
+export { GENERIC_FX_EMITTER_DESCRIPTOR_KIT_VERSION, createGenericFxEmitterDescriptorKit } from "./generic-fx-emitter-descriptor-kit.js";
+export { GENERIC_PARTICLE_FIELD_DESCRIPTOR_KIT_VERSION, createGenericParticleFieldDescriptorKit } from "./generic-particle-field-descriptor-kit.js";
+export { GENERIC_SHOCKWAVE_DESCRIPTOR_KIT_VERSION, createGenericShockwaveDescriptorKit } from "./generic-shockwave-descriptor-kit.js";
+export { GENERIC_ATMOSPHERE_LAYER_DESCRIPTOR_KIT_VERSION, createGenericAtmosphereLayerDescriptorKit } from "./generic-atmosphere-layer-descriptor-kit.js";
+export { createGenericVisualFxDescriptorKits as default } from "./visual-fx-bundle.js";
